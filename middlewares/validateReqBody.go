715cb9094d383719b8e4f681a4e57d50863e8eb7
M Fitrah Ramadhan <tgcfitrah26@gmail.com>
2022-10-01 16:31:32 +0700

    validate request body middleware implemented

--- middlewares/validateReqBody.go
+++ middlewares/validateReqBody.go
@@ -1,1 +1,24 @@
 package middlewares
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/mfitrahrmd420/FGA_Hacktiv8-Assignment2/helper"
+	"github.com/mfitrahrmd420/FGA_Hacktiv8-Assignment2/repository/postgres/order"
+)
+
+func ValidatePostOrder(ctx *gin.Context) {
+	var reqBody order.Order
+
+	if err := ctx.ShouldBindJSON(&reqBody); err != nil {
+		ctx.Error(helper.INVALID_REQUEST_BODY)
+		ctx.Abort()
+
+		return
+	}
+
+	ctx.Set("reqBody", reqBody)
+}
+
+func ValidatePutOrder(ctx *gin.Context) {
+	ValidatePostOrder(ctx)
+}
