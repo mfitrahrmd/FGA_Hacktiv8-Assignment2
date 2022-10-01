83d00e8d728a2f4f43c83647d145f767951b38f4
M Fitrah Ramadhan <tgcfitrah26@gmail.com>
2022-10-01 16:35:39 +0700

    group the order routes

--- main.go
+++ main.go
@@ -6,13 +6,12 @@ import (
 	"github.com/mfitrahrmd420/FGA_Hacktiv8-Assignment2/config"
 	"github.com/mfitrahrmd420/FGA_Hacktiv8-Assignment2/controllers"
 	docs "github.com/mfitrahrmd420/FGA_Hacktiv8-Assignment2/docs"
+	"github.com/mfitrahrmd420/FGA_Hacktiv8-Assignment2/middlewares"
+	"github.com/mfitrahrmd420/FGA_Hacktiv8-Assignment2/repository/postgres/item"
+	"github.com/mfitrahrmd420/FGA_Hacktiv8-Assignment2/repository/postgres/order"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
 	"log"
-)
-
-const (
-	PORT = 80
 )
 
 // @title Orders API
@@ -23,27 +22,36 @@ const (
 // @contact.email soberkoder@swagger.io
 // @licence.name Apache 2.0
 // @licence.url http://www.apache.org/licence/LICENCE-2.0.html
-// @host localhost:80
+// @host localhost:8080
 // @BasePath /
 func main() {
-	//change argument for below func to your database connection string
-	db := config.GetDB(fmt.Sprintf("postgres://developer:developer@localhost:5432/fga_hacktiv8_assignment2?sslmode=disable"))
+	// run 'go run main.go --help' to see required flag for database connection string
+	db := config.GetDB(fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", config.PGUSER, config.PGPASSWORD, config.PGHOST, config.PGPORT, config.PGDB))
 
-	//db.Migrate(item.Item{}, order.Order{}) // uncomment this line if run this for the first time to migrate the db
+	db.Migrate(order.Order{}, item.Item{}) // uncomment this line if run this for the first time to migrate the db
 
+	// instantiate order controller
 	orderController := controllers.NewOrderController(db.Pg)
 
-	r := gin.Default()
+	// instantiate gin router
+	r := gin.New()
 
 	// localhost/swagger/index.html for swagger API documentation
+	// BUG : cannot try Swagger UI that require path parameter, i.e : POST orders that require orderId as path parameter
 	docs.SwaggerInfo.BasePath = "/"
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
-	// handle request
-	r.POST("/orders", orderController.CreateOrder)
-	r.GET("/orders", orderController.GetOrders)
-	r.PUT("/orders/:id", orderController.UpdateOrder)
-	r.DELETE("/orders/:id", orderController.DeleteOrder)
+	// middlewares
+	r.Use(middlewares.ErrorHandler())
 
-	log.Fatal(r.Run(fmt.Sprintf(":%d", PORT)))
+	// order route handler
+	// NOTE : every error that occurs in these controllers will be thrown to error handler middleware, see 'middlewares' folder for the implementation
+	rOrder := r.Group("/orders")
+	rOrder.POST("", middlewares.ValidatePostOrder, orderController.CreateOrder)
+	rOrder.GET("", orderController.GetOrders)
+	rOrder.PUT("/:id", middlewares.ValidatePutOrder, orderController.UpdateOrder)
+	rOrder.DELETE("/:id", orderController.DeleteOrder)
+
+	// serve the server
+	log.Fatal(r.Run(fmt.Sprintf("localhost:%d", config.PORT)))
 }
